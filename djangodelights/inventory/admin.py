d67ba7f090b9408dd6220c9ff8798b9a75e96b08
Jeff-Danowski <jeff.r.danowski@gmail.com>
2024-02-08 09:14:03 -0500

    Registered models

--- djangodelights/inventory/admin.py
+++ djangodelights/inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import RecipeRequirements, MenuItem, Ingredient, Purchase
 
 # Register your models here.
+admin.site.register(RecipeRequirements)
+admin.site.register(MenuItem)
+admin.site.register(Ingredient)
+admin.site.register(Purchase)
